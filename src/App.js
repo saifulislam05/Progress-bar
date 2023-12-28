f9b57f95b37d1d81c9ce0806f11d430118aa0bf6
saifulislam05 <saiful.bijulia@gmail.com>
2023-12-28 14:42:27 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+
+function App() {
+
+  return (
+      <div className="w-screen h-screen overflow-hidden flex justify-center items-center">
+        
+      </div>
+
+  );
+}
+
+export default App;
