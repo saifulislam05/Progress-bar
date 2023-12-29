353e9901c559cc3abc9a4a8fa16bcf60b2be4ba0
saifulislam05 <saiful.bijulia@gmail.com>
2023-12-29 22:41:33 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProgressBar from "./Components/ProgressBar";
 
-
 function App() {
+  const [value, setValue] = useState(2);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((prevValue) => (prevValue >= 100 ? 100 : prevValue + 1));
+    }, 300);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="w-screen h-screen overflow-hidden flex justify-center items-center">
@@ -11,8 +18,16 @@ function App() {
         <h1 className="text-center text-3xl mb-6 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black">
           Progress bar
         </h1>
-        <ProgressBar />
-        <h2 className="text-center">Loading<span className="animate-dotty text-white 1s ease-in"></span></h2>
+        <ProgressBar value={value} />
+        <h2 className="text-center">
+          {value !== 100 ? (
+            <>
+              Loading<span className="dotty text-white 1s ease-in"></span>
+            </>
+          ) : (
+            "Completed"
+          )}
+        </h2>
       </div>
     </div>
   );
